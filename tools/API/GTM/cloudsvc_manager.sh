fbd20001b04a629b40c0ab2945bfd685a19ae4a2
dev-container-jc <dev-container@gmail.com>
2020-03-29 06:32:52 +0000

    compeleted cloudservice shell

--- tools/API/GTM/cloudsvc_manager.sh
+++ tools/API/GTM/cloudsvc_manager.sh
@@ -43,43 +43,53 @@ then
   elif [ "$2" ==  "countries" ]
   then
     echo -e "\033[32mGetting country info ................................... \033[0m "
-    curl -sk -H "Accept: application/json" -H "Authorization: Bearer $token" -X GET https://api.cloudservices.f5.com/v1/svc-geo/$2 | jq -r .
+    curl -sk -H "Accept: application/json" -H "Authorization: Bearer $token" -X GET "https://api.cloudservices.f5.com/v1/svc-geo/$2" | jq -r .
     echo -e "\033[32m ----------------------------------------------------\033[0m "
 
   elif [ "$2" ==  "regions" ]
   then
-    echo -e "\033[32mGetting region info ................................... \033[0m "
-    curl -sk -H "Accept: application/json" -H "Authorization: Bearer $token" -X GET https://api.cloudservices.f5.com/v1/svc-geo/$2 | jq -r .
-    echo -e "\033[32m ----------------------------------------------------\033[0m "
+    echo -e "\033[32mGetting region info ........................................ \033[0m "
+    curl -sk -H "Accept: application/json" -H "Authorization: Bearer $token" -X GET "https://api.cloudservices.f5.com/v1/svc-geo/$2" | jq -r .
+    echo -e "\033[32m-------------------------------------------------------------\033[0m "
 
   elif [ "$2" ==  "subscription" ]
   then
     echo -e "\033[32mGetting subscription info .................................. \033[0m "
-    curl -sk -H "Accept: application/json" -H "Authorization: Bearer $token" -X GET "https://api.cloudservices.f5.com/v1/svc-subscription/subscriptions?status=_allStatusFilter&account_id=$id" | jq -r .
-    echo -e "\033[32m ----------------------------------------------------\033[0m "
-  else
-    echo -e "\033[32mYou need to type what you want to see item\033[0m "
+    count=$(curl -sk -H "Accept: application/json" -H "Authorization: Bearer $token" -X GET "https://api.cloudservices.f5.com/v1/svc-subscription/subscriptions?status=_allStatusFilter&account_id=$id" | jq -r '.subscriptions | length')
+    if [ count > 1 ]
+    then  
+      for (( i=0; i < $count; i++ ))
+      do
+      sub_id=$(curl -sk -H "Accept: application/json" -H "Authorization: Bearer $token" -X GET "https://api.cloudservices.f5.com/v1/svc-subscription/subscriptions?status=_allStatusFilter&account_id=$id" | jq -r '.subscriptions['$i'].subscription_id')
+      sub_name=$(curl -sk -H "Accept: application/json" -H "Authorization: Bearer $token" -X GET "https://api.cloudservices.f5.com/v1/svc-subscription/subscriptions?status=_allStatusFilter&account_id=$id" | jq -r '.subscriptions['$i'].service_instance_name')
+      echo -e "Subscription name is \033[32m$sub_name\033[0m and ID is \033[32m$sub_id\033[0m"
+      done
+    fi
+    echo -e "\033[32m-----------------------------------------------------------\033[0m "
+
+  #else
+  #  echo -e "\033[32mYou need to type what you want to see item\033[0m "
   fi
 
 elif [ "$1" ==  "create" ]
 then
-  if [ "$2" ==  "pool" ]
-  then
-    echo -e "\033[32mCreating new GSLB Policy .................................... \033[0m "
-    #curl -sk -H "Accept: application/json" -H "Authorization: Bearer $token" -X get "https://api.cloudservices.f5.com/v1/svc-subscription/subscriptions?status=_allStatusFilter&account_id=$id" | jq -r .
-    echo -e "\033[32m ----------------------------------------------------\033[0m "
-  else
-    echo -e "\033[32mYou need to type what you want to modify item\033[0m "
-  fi
+  echo -e "\033[32mCreating GSLB Services ..................................... \033[0m "
+  curl -sk -H "Accept: application/json" -H "Authorization: Bearer $token" -X POST -d @$2 "https://api.cloudservices.f5.com/v1/svc-subscription/subscriptions" | jq -r .
+  echo -e "\033[32m-------------------------------------------------------------\033[0m "
 
-elif [ "$1" ==  "modify" ]
+elif [ "$1" ==  "activate" ]
 then
-  if [ "$2" ==  "pool" ]
-  then
-    echo -e "\033[32mModifying GSLB Policy ....................................... \033[0m "
-    echo -e "\033[32m ----------------------------------------------------\033[0m "
-  else
-    echo -e "\033[32mYou need to type what you want to modify item\033[0m "
-  fi
+  SUBSCRIPTION_ID=$2
+  echo -e "\033[32mActivating GSLB Service....................................... \033[0m "
+  curl -sk -H "Accept: application/json" -H "Authorization: Bearer $token" -X POST "https://api.cloudservices.f5.com/v1/svc-subscription/subscriptions/$SUBSCRIPTION_ID/activate" | jq -r .
+  echo -e "\033[32m-------------------------------------------------------------\033[0m "
+
+elif [ "$1" ==  "delete" ]
+then
+  SUBSCRIPTION_ID=$2
+  echo -e "\033[32mDeleting GSLB Services ..................................... \033[0m "
+  curl -sk -H "Accept: application/json" -H "Authorization: Bearer $token" -X POST "https://api.cloudservices.f5.com/v1/svc-subscription/subscriptions/$SUBSCRIPTION_ID/retire" | jq -r .
+  echo -e "\033[32m-------------------------------------------------------------\033[0m "
+
 
 fi
---
+++ tools/API/GTM/demo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in {1..200}
+do
+
+  curl http://www.gslb.cloud5demo.com
+  echo "------------- $i -----------"
+  sleep 1
+
+done
+
